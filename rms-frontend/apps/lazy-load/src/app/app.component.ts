2c92435f0e4ce58248e893c20566e35d04a24404
Kaylee Oh <saepark90@gmail.com>
2020-04-06 19:53:10 -0400

    RMSL-14 update read me file for lazy loadin

--- rms-frontend/apps/lazy-load/src/app/app.component.ts
+++ rms-frontend/apps/lazy-load/src/app/app.component.ts
@@ -1,40 +1,18 @@
-/**
- * Lazy loading components
- * REFERENCE EXAMPLE
- * https://medium.com/angular-in-depth/lazy-load-components-in-angular-596357ab05d8
- * 
- * ADVANTAGE OF LAZY LOAD
- * * less components loaded which will enhance performance when application starts to get larger
- * * smaller bundle size
- * * as application grows lazy loading component will 
- * reduce the size of the javascript file ehanceing performance
- * 
- * PLACES WE CAN USE LAZY LOADING COMPONENT
- * could use this in our dynamic forms reduce amount of unused components to be imported each time
- * 
- * 
- * THINGS TO KEEP IN MIND WHEN LAZY LOADING COMPONENT
- * life cycle hooks:
- * * when lazy loading component ngOnChange life cycle hook doesn't get triggered.
- * therefore when properties are changed we have manually call ngOnChanges life cycle hook.
- * 
- */
 import { Component, ViewContainerRef, ComponentFactoryResolver, ViewChild, Injector, OnInit, AfterViewInit } from '@angular/core';
-import { FileExplorerComponent } from 'libs/file-explorer/src/lib/file-explorer/file-explorer.component';
-// import { Part1Component } from './part1/part1.component';
 
 @Component({
   selector: 'rms-frontend-lazy-load',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'lazy-load';
 
   @ViewChild('btnContainer', { read: ViewContainerRef }) btnContainer: ViewContainerRef;
   @ViewChild('container1', { read: ViewContainerRef }) container1: ViewContainerRef;
   @ViewChild('container2', { read: ViewContainerRef }) container2: ViewContainerRef;
-  @ViewChild('container3', { read: ViewContainerRef }) container3: ViewContainerRef;  
+  @ViewChild('container3', { read: ViewContainerRef }) container3: ViewContainerRef;
 
   constructor(
     private cfr: ComponentFactoryResolver,
@@ -64,11 +42,13 @@ export class AppComponent implements OnInit, AfterViewInit {
   async lazyLoadFileExplorerModule() {
     import('@rms-frontend/file-explorer').then(
       ({ FileExplorerModule }) => {
-        const component = this.cfr.resolveComponentFactory(FileExplorerComponent);
-        const componentRef = this.container3.createComponent(component);
-        // componentRef.instance.
-      }
-    )
+        import('libs/file-explorer/src/lib/file-explorer/file-explorer.component').then(
+          ({ FileExplorerComponent }) => {
+            const component = this.cfr.resolveComponentFactory(FileExplorerComponent);
+            const componentRef = this.container3.createComponent(component);
+          }
+        )
+      })
   }
 
   async getLazy1() {
@@ -84,7 +64,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   async getLazy2() {
-    this.btnContainer.clear(); // clear existing component
+    this.btnContainer.clear(); // clear existing component in the container
     const { Part3Component } = await import('./part3/part3.component');
     this.btnContainer.createComponent(
       this.cfr.resolveComponentFactory(Part3Component)
@@ -117,7 +97,7 @@ export class AppComponent implements OnInit, AfterViewInit {
     import('./tab-content1/tab-content1.component').then(
       ({ TabContent1Component }) => {
         const component = this.cfr.resolveComponentFactory(TabContent1Component);
-        const componentRef = this.container1.createComponent(component);        
+        const componentRef = this.container1.createComponent(component);
       }
     );
   }
