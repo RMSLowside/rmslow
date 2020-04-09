94ee58eb02c4133dbc14200a73d96a1dce3bbbb1
Daniel Kolb <flare891@gmail.com>
2020-04-09 13:44:09 -0400

    RMSL-18 fixing several issues, adding a preprocessor of sorts (does op switch before term boundary setting)

--- Adv-Search/search/src/main/java/app/Search.java
+++ Adv-Search/search/src/main/java/app/Search.java
@@ -4,13 +4,35 @@ import java.io.Console;
 
 public class Search {
 
-    public static String termify(final String inputString){
+    public static String termify(final String input){
         String resultString = "";
+        String restrictedChars = "&|!() ";
         boolean termOpen = false;
         boolean quoteTermOpen = false;
 
+        //------------Preprocessing--------------
+        // Trim extra spaces on the ends
+        String inputString = input.trim();
+        //pre-emptively replace the quoted operators
+        for(int i = 0; i < inputString.length(); i++){
+            if(!quoteTermOpen && inputString.charAt(i) == '"' && (i == 0 || inputString.charAt(i-1) != '\\')){
+                quoteTermOpen = !quoteTermOpen;
+            }
+            else if(quoteTermOpen && inputString.charAt(i) == '"' && (i == 0 || inputString.charAt(i-1) != '\\')) {
+                quoteTermOpen = !quoteTermOpen;
+            }
+            else {
+                //check if character is an operator, replace with placeholder if inside a term
+                if((quoteTermOpen) && "&|!".indexOf(inputString.charAt(i)) != -1){
+                    if(inputString.charAt(i) == '&') inputString = inputString.substring(0, i) + "<:amp:>" + inputString.substring(i+1);
+                    if(inputString.charAt(i) == '|') inputString = inputString.substring(0, i) + "<:pipe:>" + inputString.substring(i+1);
+                    if(inputString.charAt(i) == '!') inputString = inputString.substring(0, i) + "<:excl:>" + inputString.substring(i+1);
+                }
+            }
+        }
+
         //first check if there are no operators - if that's true, the whole text is one term.
-        if(!inputString.contains(" & ") && !inputString.contains(" | ") && !inputString.contains(" ! ")){
+        if(!inputString.contains("&") && !inputString.contains("|") && !inputString.contains("!") && !inputString.contains("\"")){
             return "<term>" + inputString + "</term>";
         }
 
@@ -24,24 +46,21 @@ public class Search {
                 quoteTermOpen = !quoteTermOpen;
                 resultString += "</term>";
             }
-            else if(!quoteTermOpen && !termOpen && "&|!() ".indexOf(inputString.charAt(i)) == -1){
-                termOpen = !termOpen;
+            else if(!quoteTermOpen && !termOpen && restrictedChars.indexOf(inputString.charAt(i)) == -1){
+                // termOpen = !termOpen;
                 resultString += "<term>" + inputString.charAt(i);
+                if(i+1 == inputString.length() || restrictedChars.indexOf(inputString.charAt(i+1)) != -1){
+                    resultString += "</term>";
+                }
+                else termOpen = !termOpen;
             }
-            else if(!quoteTermOpen && termOpen && (i+1 == inputString.length() || ") ".indexOf(inputString.charAt(i+1)) != -1)){
+            else if(!quoteTermOpen && termOpen && 
+                    (i+1 == inputString.length() || restrictedChars.indexOf(inputString.charAt(i+1)) != -1)){
                 termOpen = !termOpen;
                 resultString += inputString.charAt(i) + "</term>";
             }
             else {
-                //check if character is an operator, replace with placeholder if inside a term
-                if((quoteTermOpen || termOpen) && "&|!".indexOf(inputString.charAt(i)) != -1){
-                    if(inputString.charAt(i) == '&') resultString += "<:amp:>";   //I believe this part already happens in the real thing, mid-processing
-                    if(inputString.charAt(i) == '|') resultString += "<:pipe:>";
-                    if(inputString.charAt(i) == '!') resultString += "<:excl:>";
-                }
-                else {
-                    resultString += inputString.charAt(i);
-                }
+                resultString += inputString.charAt(i);
             }
         }
 
