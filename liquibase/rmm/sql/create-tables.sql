3f74e596286e2a7e435d9a5a63435811b8307b2a
Adam Kimmel <adampkimmel@gmail.com>
2020-04-14 10:33:15 -0400

    RMSL-48 Create Liquibase scripts

---
+++ liquibase/rmm/sql/create-tables.sql
@@ -0,0 +1,20 @@
+--liquibase formatted sql
+
+--changeset kimmela:rmsl-48
+create table records (
+    guide varchar(255) primary key,
+    ruleId int NOT NULL,
+    rcsId int NOT NULL,
+    decisionDate date NOT NULL,
+    documentCreateDate date NOT NULL,
+    dispositionDate date NOT NULL,
+    INDEX rule (ruleId),
+    INDEX rcs (rcsId),
+    INDEX disposition (dispositionDate)
+)
+
+create table holds (
+    guide varchar(255) primary key,
+    releaseDate date,
+    INDEX release (releaseDate)
+)
