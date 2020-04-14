a1110ed023ccb63bf6fb75c2d7a3960a005b31b0
Adam Kimmel <adampkimmel@gmail.com>
2020-04-14 11:58:30 -0400

    More

--- liquibase/rmm/sql/v1_0.sql
+++ liquibase/rmm/sql/v1_0.sql
@@ -3,20 +3,22 @@
 --changeset kimmela:rmsl-48
 USE rmm;
 
-CREATE table records (
+CREATE TABLE records (
     guide varchar(255) primary key,
     ruleId int NOT NULL,
     rcsId int NOT NULL,
     decisionDate date NOT NULL,
     documentCreateDate date NOT NULL,
-    dispositionDate date NOT NULL,
-    INDEX rule (ruleId),
-    INDEX rcs (rcsId),
-    INDEX disposition (dispositionDate)
-)
+    dispositionDate date NOT NULL
+);
 
-CREATE table holds (
+CREATE INDEX rule ON records (ruleId);
+CREATE INDEX rcs ON records (rcsId);
+CREATE INDEX disposition ON records (dispositionDate);
+
+CREATE TABLE holds (
     guide varchar(255) primary key,
-    releaseDate date,
-    INDEX release (releaseDate)
-)
+    releaseDate date NOT NULL
+);
+
+CREATE INDEX releases ON holds (releaseDate);
