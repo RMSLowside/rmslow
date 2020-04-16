3f609ffc5a2b7bddc43451b244e1f31e3afb795d
Adam Kimmel <adampkimmel@gmail.com>
2020-04-16 08:53:27 -0400

    More work

--- NiFi/rms-custom-processors/src/main/java/rms/processors/http/FetchXmlFromUrlProcessor.java
+++ NiFi/rms-custom-processors/src/main/java/rms/processors/http/FetchXmlFromUrlProcessor.java
@@ -1,8 +1,10 @@
 package rms.processors.http;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.nifi.annotation.documentation.Tags;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.processor.DataUnit;
 import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.ProcessSession;
@@ -10,11 +12,19 @@ import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.util.StandardValidators;
 import org.apache.nifi.stream.io.StreamUtils;
+import rms.models.PublisherJsonMessage;
 import rms.processors.AbstractRmsProcessor;
 
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static rms.processors.utilities.RmsEnums.ATTRIBUTE_IS_VALID;
+import static rms.processors.utilities.RmsEnums.ATTRIBUTE_VALIDATION_ERRORS;
 
 @Tags({"rms"})
 public class FetchXmlFromUrlProcessor extends AbstractRmsProcessor {
@@ -49,26 +59,74 @@ public class FetchXmlFromUrlProcessor extends AbstractRmsProcessor {
         descriptors.add(CHARACTER_SET);
     }
 
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final ArrayList<String> producers = new ArrayList<>(Arrays.asList("Amazon", "Alphabet", "Microsoft", "Apple", "SpaceX", "Walmart", "Costco", "UnitedHealth"));
+    private static final String RANDOM_XML = "<documentRoot desVersion='5' type='fake'><guide>rms.guide</guide><producer>rms.producer</producer><createDate>rms.date</createDate></documentRoot>";
+
     @Override
     public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
+        final ComponentLog log = getLogger();
+
         FlowFile flowFile = session.get();
         if (flowFile == null) {
             return;
         }
 
         String content = getFlowFileContent(flowFile, context, session);
+        PublisherJsonMessage message;
 
-        // TODO convert to JSON
+        try {
+            message = mapper.readValue(content, PublisherJsonMessage.class);
+        } catch (Exception e) {
+            log.error("Failed to map text to expected POJO due to " + e.getMessage());
 
-        // TODO get URL from JSON
+            session.putAttribute(flowFile, ATTRIBUTE_IS_VALID, "false");
+            session.putAttribute(flowFile, ATTRIBUTE_VALIDATION_ERRORS, e.getMessage());
+            session.transfer(flowFile, REL_FAILURE);
 
-        //TODO fetch XML from URL
+            return;
+        }
 
-        // TODO make Flow FIle content the XML
+        String url = message.getDocUrl();
 
-        // TODO handle failures by retrying a few times
+        // TODO fetch XML from URL. For now just use random XML
+        log.info("Fetching document from URL: " + url);
+        // Set guide
+        String aGuide = UUID.randomUUID().toString();
+        // Set producer
+        int randomIndex = (int) (Math.random() * producers.size());
+        String aProducer = producers.get(randomIndex);
+        // Set date
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date firstDate;
+        Date lastDate;
+        try {
+            firstDate = sdf.parse("1950-01-01");
+            lastDate = sdf.parse("2020-01-01");
+        } catch (ParseException e) {
+            log.error("Failed to generate dates due to " + e.getMessage());
 
-        session.transfer(flowFile, REL_SUCCESS);
+            session.putAttribute(flowFile, ATTRIBUTE_IS_VALID, "false");
+            session.putAttribute(flowFile, ATTRIBUTE_VALIDATION_ERRORS, e.getMessage());
+            session.transfer(flowFile, REL_FAILURE);
+
+            return;
+        }
+        long random = ThreadLocalRandom.current().nextLong(firstDate.getTime(), lastDate.getTime());
+        Date date = new Date(random);
+
+        // Insert values into XML
+        log.info("VALUES [" + aGuide + ", " + aProducer + ", " + sdf.format(date) + "]");
+        String output = RANDOM_XML.replace("rms.guide", aGuide).replace("rms.producer", aProducer).replace("rms.date", sdf.format(date));
+
+        // Write the XML to a flow file
+        FlowFile outputFlowFile = session.write(flowFile, (in, out) -> {
+            try (OutputStream outputStream = new BufferedOutputStream(out)) {
+                outputStream.write(output.getBytes());
+            }
+        });
+
+        session.transfer(outputFlowFile, REL_SUCCESS);
     }
 
     private String getFlowFileContent(FlowFile flowFile, ProcessContext context, ProcessSession session) {
