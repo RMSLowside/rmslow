1f1faff1a7f4922d613d8d5bca548d03e3c4db4f
Steve McFall <smcfall215@gmail.com>
2020-04-23 11:31:55 -0400

    RMSL-58 all tests pass, but not good coverage

--- rms-frontend/apps/lazy-load/src/app/home/home.component.spec.ts
+++ rms-frontend/apps/lazy-load/src/app/home/home.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { CoreModule } from '@rms-frontend/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Part1Component } from './part1/part1.component';
+import { TabContent1Component } from './tab-content1/tab-content1.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +13,13 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [HomeComponent, Part1Component, TabContent1Component],
+      imports: [CoreModule, BrowserModule, BrowserAnimationsModule]
     })
-    .compileComponents();
+      .overrideModule(BrowserModule, {
+        set: { entryComponents: [Part1Component, TabContent1Component] }
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +31,14 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`lazyLoadComponent1 and lazyLoadTabContent1 should have been called`, async(() => {
+    spyOn(component, 'lazyLoadComponent1');
+    spyOn(component, 'lazyLoadTabContent1');
+    fixture.detectChanges();
+    component.ngAfterViewInit();
+
+    expect(component.lazyLoadComponent1).toHaveBeenCalled();
+    expect(component.lazyLoadTabContent1).toHaveBeenCalled();
+  }));
 });
