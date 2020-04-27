6d73b30c365f656d56835e5669cd4626fc1181e4
Adam Kimmel <adampkimmel@gmail.com>
2020-04-27 11:52:39 -0400

    RMSL-93 Added controller and processor to log Data Procenance information

--- scripts/ec2/setupEc2.sh
+++ scripts/ec2/setupEc2.sh
@@ -31,9 +31,12 @@ yum install -y apache-maven
 aws s3 cp s3://rmslowdeployment/software/nifi-1.11.4-bin.tar.gz .
 tar -xzvf nifi-1.11.4-bin.tar.gz
 rm nifi-1.11.4-bin.tar.gz
-mv nifi-1.11.4 nifi
+mv nifi-1.11.4 nifi0
 sed -i 's/-Xms512m/-Xms2048m/g' nifi/conf/bootstrap.conf
 sed -i 's/-Xmx512m/-Xmx2048m/g' nifi/conf/bootstrap.conf
+sed -i 's/nifi.remote.input.host=/nifi.remote.input.host=localhost/g' nifi/conf/nifi.properties
+sed -i 's/nifi.remote.input.socket.port=/nifi.remote.input.socket.port=10000/g' nifi/conf/nifi.properties
+sed -i 's/nifi.remote.input.http.enabled=false/nifi.remote.input.http.enabled=true/g' nifi/conf/nifi.properties
 cp rmslow/NiFi/flows/flow.xml.gz nifi/conf/flow.xml.gz
 pushd rmslow/NiFi
 mvn clean install
