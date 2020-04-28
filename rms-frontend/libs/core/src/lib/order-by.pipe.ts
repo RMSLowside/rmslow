d50de8103eee8243f2d79e759d10582ee0ad91fe
Steve McFall <smcfall215@gmail.com>
2020-04-28 08:55:06 -0400

    RMS:-65 ordering things

--- rms-frontend/libs/core/src/lib/order-by.pipe.ts
+++ rms-frontend/libs/core/src/lib/order-by.pipe.ts
@@ -6,19 +6,23 @@ import orderBy from 'lodash-es/orderBy';
   name: 'orderBy'
 })
 export class OrderByPipe implements PipeTransform {
-  transform(value: any[], desc: boolean, column: string = ''): any[] {
+  transform(value: any[], desc: boolean, column): any[] {
     if (!value) {
       return value;
     } // no array
 
     if (!column || column === '') {
-      return sortBy(value);
+      return sortBy(value.toString().toLowerCase());
     } // sort 1d array
 
     if (value.length <= 1) {
       return value;
     } // array with only one item
 
-    return orderBy(value, [column], [desc]);
+    return orderBy(
+      value,
+      [val => val[column].toString().toLowerCase()],
+      [desc]
+    );
   }
 }
