952a9de54b297c111d10a2922b2e4946c6b3135a
Steve McFall <smcfall215@gmail.com>
2020-05-05 10:41:58 -0400

    RMSL-85 add filters and sort

--- rms-frontend/libs/version-history/src/lib/history-list/history-list.component.ts
+++ rms-frontend/libs/version-history/src/lib/history-list/history-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { VersionHistory } from '../version-history';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'rms-frontend-history-list',
@@ -9,7 +10,28 @@ import { VersionHistory } from '../version-history';
 export class HistoryListComponent implements OnInit {
   @Input() histories: VersionHistory[];
   @Input() canEdit: boolean;
+  fieldFilter = new FormControl();
+  userFilter = new FormControl();
+  sort = new FormControl();
   constructor() {}
 
   ngOnInit(): void {}
+
+  getFilter(filter: string) {
+    return [...new Set(this.histories.map(a => a[filter]))];
+  }
+  filterHistories() {
+    let filteredHistories = this.histories;
+    const fieldFilters = this.fieldFilter.value;
+    const userFilters = this.userFilter.value;
+    if (fieldFilters?.length)
+      filteredHistories = filteredHistories.filter(a =>
+        fieldFilters.includes(a.field)
+      );
+    if (userFilters?.length)
+      filteredHistories = filteredHistories.filter(a =>
+        userFilters.includes(a.user)
+      );
+    return filteredHistories;
+  }
 }
--- rms-frontend/libs/version-history/src/lib/version-history.module.ts
+++ rms-frontend/libs/version-history/src/lib/version-history.module.ts
@@ -4,10 +4,15 @@ import { CoreModule } from '@rms-frontend/core';
 import { HistoryListComponent } from './history-list/history-list.component';
 import { HistorySegmentComponent } from './history-segment/history-segment.component';
 import { HistoryModalComponent } from './history-modal/history-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, CoreModule],
-  declarations: [HistoryListComponent, HistorySegmentComponent, HistoryModalComponent],
+  imports: [CommonModule, CoreModule, ReactiveFormsModule],
+  declarations: [
+    HistoryListComponent,
+    HistorySegmentComponent,
+    HistoryModalComponent
+  ],
   exports: [HistoryListComponent]
 })
 export class VersionHistoryModule {}
