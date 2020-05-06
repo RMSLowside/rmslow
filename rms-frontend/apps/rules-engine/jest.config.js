dfc45bfb7fb30876df6bfce413e0c57cfca00c35
Adam Kimmel <adampkimmel@gmail.com>
2020-05-06 14:14:48 -0400

    Checkin for the night

--- rms-frontend/apps/rules-engine/jest.config.js
+++ rms-frontend/apps/rules-engine/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 's3',
+  name: 'Rules Engine',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/s3',
+  coverageDirectory: '../../coverage/apps/rules-engine',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
